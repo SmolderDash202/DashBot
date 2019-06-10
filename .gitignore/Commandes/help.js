633b541a9257884e9116018e90bec94c448f3962
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-06-10 19:23:46 +0200

    Update help.js

--- .gitignore/Commandes/help.js
+++ .gitignore/Commandes/help.js
@@ -6,7 +6,7 @@ module.exports.run = async(client, message, args) => {
         embed:{
             color: 0xffffff,
             title: "Liste des commandes disponible",
-            description:"Le prefix du bot est `?`\n\n**:shield: Modération**\n`purge` efface un nombre de message\n`kick` kick un membre\n`ban` ban un membre\n`mute` mute un membre\n`unmute` Unmute un joueur mute\n`sondage` crér un sondange\n\n**:ferris_wheel: Fun**\n`cat`affiche une immage aléatoire de chat\n`dog`Affiche une image aléatoire de chien\n`8ball` permet de poser une question au bot\n\n**:satellite: Utile**\n`help` affiche l'aide\n`ping` affiche la latence du bot\n`stats` affiche les stats d'un joueur",
+            description:"Le prefix du bot est `?`\n\n**:shield: Modération**\n`clear` efface un nombre de message\n`kick` kick un membre\n`ban` ban un membre\n`mute` mute un membre\n`unmute` Unmute un joueur mute\n`sondage` crér un sondange\n\n**:ferris_wheel: Fun**\n`cat`affiche une immage aléatoire de chat\n`dog`Affiche une image aléatoire de chien\n`8ball` permet de poser une question au bot\n\n**:satellite: Utile**\n`help` affiche l'aide\n`ping` affiche la latence du bot\n`stats` affiche les stats d'un joueur",
             footer:{
                 text:"©Tout droit résérvé à vcoding#4488 et SmolderDash202#8101 - Version 1.0"
             }
