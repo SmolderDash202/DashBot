970713e54408c60ef7ec2991472df4db858c9477
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-06-11 19:56:52 +0200

    Create rolldice.js

---
+++ .gitignore/Commandes/rolldice.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js"),
+      superagent = require("superagent");
+
+module.exports.run = async(client, message, args) => {
+
+    let msg = await message.channel.send("Génération...");
+
+    let {body} = await superagent.get(`https://dice-roll-koisusimkz.now.sh/d6`);
+    if(!{body}) return message.channel.send(":x: Erreur - Une erreur inconnue est survenue.");
+
+    message.channel.send(`Voici le resultat du lancé de dé : **${body.rolls}**`);
+    msg.delete();
+};
+
+module.exports.help = {
+    name:"roll"
+};
