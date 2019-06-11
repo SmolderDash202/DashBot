41a09f8db5f072b7fab3e160e8f58ea1aa53b2d5
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-06-11 18:57:46 +0200

    Update randomscat.js

--- .gitignore/Commandes/randomscat.js
+++ .gitignore/Commandes/randomscat.js
@@ -1,18 +1,24 @@
 const Discord = require("discord.js"),
-      random = require('random-cat');
+      superagent = require("superagent");
 
 module.exports.run = async(client, message, args) => {
 
-    var url = random.get();
+    let msg = await message.channel.send("Génération...");
+
+    let {body} = await superagent.get(`http://aws.random.cat/meow`);
+    if(!{body}) return message.channel.send(":x: Erreur - Une erreur inconnue est survenue.");
+
     message.channel.send({
-        embed:{
-            color:0xffffff,
-            title:`Voici le chat que ${message.author.username} a demandé.`,
-            image: {
-                url: `${url}`
+        embed: {
+            color: 0xffffff,
+            title: "Chat aléatoire",
+            description:`Voici le chat que **${message.author.username}** a demandé`,
+            image:{
+                url: `${body.file}`
             }
         }
     });
+    msg.delete();
 };
 
 module.exports.help = {
