a30a0763790f52ede088becb1173997550e01af8
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-06-11 18:58:07 +0200

    Update randomsdog.js

--- .gitignore/Commandes/randomsdog.js
+++ .gitignore/Commandes/randomsdog.js
@@ -1,19 +1,24 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"),
+      superagent = require("superagent");
 
 module.exports.run = async(client, message, args) => {
 
+    let msg = await message.channel.send("Génération...");
+
+    let {body} = await superagent.get(`https://dog.ceo/api/breeds/image/random`);
+    if(!{body}) return message.channel.send(":x: Erreur - Une erreur inconnue est survenue.");
+
     message.channel.send({
-        embed:{
-            color:0xffffff,
-            title:`Voici le chien que ${message.author.username} a demandé.`,
-            image: {
-                url: "https://i.imgur.com/MxeLpuF.jpg"
-            },
-            footer:{
-                text:"?dog est encore en développement !"
+        embed: {
+            color: 0xffffff,
+            title: "Chat aléatoire",
+            description:`Voici le chat que **${message.author.username}** a demandé`,
+            image:{
+                url: `${body.message}`
             }
         }
     });
+    msg.delete();
 };
 
 module.exports.help = {
