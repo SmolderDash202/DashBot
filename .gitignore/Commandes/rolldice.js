910a02ec25251c1817af0ed10b2659bba8a5c79b
Vincent P <45183326+vincent-coding@users.noreply.github.com>
2019-06-12 15:35:18 +0200

    Update rolldice.js

--- .gitignore/Commandes/rolldice.js
+++ .gitignore/Commandes/rolldice.js
@@ -7,8 +7,74 @@ module.exports.run = async(client, message, args) => {
 
     let {body} = await superagent.get(`https://dice-roll-koisusimkz.now.sh/d6`);
     if(!{body}) return message.channel.send(":x: Erreur - Une erreur inconnue est survenue.");
-
-    message.channel.send(`Voici le resultat du lancé de dé : **${body.rolls}**`);
+    
+    if(body.rolls == 1) {
+        message.channel.send({
+            embed:{
+                color: 0xffffff,
+                title:`Résultat du lancé dé : **${body.rolls}**`,
+                image:{
+                    url:"https://i.imgur.com/JLyJ5my.png"
+                }
+            }
+        });
+    }
+    if(body.rolls == 2) {
+        message.channel.send({
+            embed:{
+                color: 0xffffff,
+                title:`Résultat du lancé dé : **${body.rolls}**`,
+                image:{
+                    url:"https://i.imgur.com/Uyubnb5.png"
+                }
+            }
+        });
+    }
+    if(body.rolls == 3) {
+        message.channel.send({
+            embed:{
+                color: 0xffffff,
+                title:`Résultat du lancé dé : **${body.rolls}**`,
+                image:{
+                    url:"https://i.imgur.com/yHaWrTP.png"
+                }
+            }
+        });
+    }
+    if(body.rolls == 4) {
+        message.channel.send({
+            embed:{
+                color: 0xffffff,
+                title:`Résultat du lancé dé : **${body.rolls}**`,
+                image:{
+                    url:"https://i.imgur.com/SnvTEwD.png"
+                }
+            }
+        });
+    }
+    if(body.rolls == 5) {
+        message.channel.send({
+            embed:{
+                color: 0xffffff,
+                title:`Résultat du lancé dé : **${body.rolls}**`,
+                image:{
+                    url:"https://i.imgur.com/wg2tUWO.png"
+                }
+            }
+        });
+    }
+    if(body.rolls == 6) {
+        message.channel.send({
+            embed:{
+                color: 0xffffff,
+                title:`Résultat du lancé dé : **${body.rolls}**`,
+                image:{
+                    url:"https://i.imgur.com/Z6c5VrB.png"
+                }
+            }
+        });
+    }
+   
     msg.delete();
 };
 
